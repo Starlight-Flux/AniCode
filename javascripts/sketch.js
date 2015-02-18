20636923b450558b03e036a36d77e93d1a4fc398
Starlight-Flux <Starlight.Flux@gmail.com>
2015-02-18 15:49:05 +0000

    Added Img_Backtrees variable

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,19 +1,19 @@
 var i = 0;
 
-//var Img_Backtrees;
+var Img_Backtrees;
 
 function setup() {
   // put setup code here
   var elliCanvas = createCanvas(200,200);
   elliCanvas.parent('elliCanvasDiv')
-  //Img_Backtrees = loadImage("parallax-forest-back-trees.png");
+  Img_Backtrees = loadImage("parallax-forest-back-trees.png");
 }
 
 function draw() {
   // put drawing code here
   ellipse(i, 46, 55, 55);
   
-  //image(img, 10, 10);
+  image(Img_Backtrees, 0, 0);
   
 }
 
