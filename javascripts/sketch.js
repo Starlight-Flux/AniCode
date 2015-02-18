f80d606ffd0b4922af3df3c719846599eb90ad40
Starlight-Flux <Starlight.Flux@gmail.com>
2015-02-18 14:51:30 +0000

    Added background image

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,12 +1,22 @@
+var i = 0;
+
+var Img_Backtrees;
+
+function preload() {
+  Img_Backtrees = loadImage("assets/parallax-forest-back-trees.png");
+}
+
 function setup() {
   // put setup code here
   var elliCanvas = createCanvas(200,200);
-  
+  var screenW = screenwidth;
   elliCanvas.parent('elliCanvasDiv')
 }
 
 function draw() {
   // put drawing code here
-  ellipse(56, 46, 55, 55);
+  ellipse(i, 46, 55, 55);
+  Img_Backtrees(img, 10, 10);
+  
 }
 
